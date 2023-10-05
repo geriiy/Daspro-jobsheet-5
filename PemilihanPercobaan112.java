d11b845af65ae24930785ac6d3ad3af2958822dc
geriiy <gheriyaj@gmail.com>
2023-10-05 11:47:27 +0700

    modifikasi1

--- PemilihanPercobaan112.java
+++ PemilihanPercobaan112.java
@@ -8,11 +8,11 @@ public static void main(String[] args) {
     
     System.out.print("Masukkan angka: ");
     int angka = input12.nextInt();
+    String Hasil;
 
-    if (angka % 2 == 0)
-        System.out.println("Angka "+angka+" bilangan genap");
-    else
-        System.out.println("Angka "+angka+" bilangan ganjil");
+    Hasil = (angka % 2 == 0) ? "Bilangan genap" : "Bilangan ganjil";
+    System.out.println(angka + " adalah " + Hasil);
+
 
 }
 
