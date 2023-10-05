a67d8907844c0e69b76a8cf4002af6fa2bd22816
geriiy <gheriyaj@gmail.com>
2023-10-05 13:06:20 +0700

    modifikasi1

--- PemilihanPercobaan212.java
+++ PemilihanPercobaan212.java
@@ -14,8 +14,31 @@ public class PemilihanPercobaan212 {
         float tugas = input12.nextFloat();
 
         float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
-        String message = total < 65 ? "Remidi" : "Tidak remidi";
-        System.out.println("Nilai akhir = " + total + " sehingga " + message);
+
+
+        if (total<=39) {
+            System.out.println("Nilai Anda adalah E");
+        } 
+        else if (total<=50) {
+            System.out.println("Nilai Anda adalah D");
+        } 
+        else if (total<=60) {
+            System.out.println("Nilai Anda adalah C");
+        } 
+        else if (total<=65) {
+            System.out.println("Nilai Anda adalah C+");
+        }
+        else if (total<=73) {
+            System.out.println("Nilai Anda adalah B");
+        }
+        else if (total<=80) {
+            System.out.println("Nilai Anda adalah B+");
+        }
+        else {
+            System.out.println("Nilai Anda adalah A");
+            
+        }
+
     }
     
 }
